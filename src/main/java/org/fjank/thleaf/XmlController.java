4cf1cc84a8a2ce687bc08d034aa6114f7533405b
Frank Karlstrøm <frank.karlstrom@atea.no>
2023-06-19 10:02:02 +0200

    Corrected the controller, changed to RestController to get the free responsebody, and made sure the getmapping actually produces application/xml.

--- src/main/java/org/fjank/thleaf/XmlController.java
+++ src/main/java/org/fjank/thleaf/XmlController.java
@@ -1,12 +1,14 @@
 package org.fjank.thleaf;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class XmlController {
-    @GetMapping(value = "/test")
+    @GetMapping(value = "/test", produces = { MediaType.APPLICATION_XML_VALUE })
     public ModelAndView test() {
         String str = """
                 The five markup-significant characters: <, >, &, " and ' All non ASCII characters.
